8255996bdbb2b11319f80965dee8a13b16d24826
Mateusz Sacha <zsuetam36@gmail.com>
2026-03-29 23:09:53 +0200

    Lab4 final version

--- loadtest/endpoints.sh
+++ loadtest/endpoints.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Endpoint URLs for load testing
 # Fill in after deploying with the URLs printed by each deploy script
-export LAMBDA_ZIP_URL="https://d363jcsdcrp3q6dzzxjikdujtm0tmurl.lambda-url.us-east-1.on.aws"        # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export LAMBDA_CONTAINER_URL="https://svxuwsjvtulqgbsqqdj2kto5z40psbyp.lambda-url.us-east-1.on.aws"  # e.g. https://<id>.lambda-url.us-east-1.on.aws
-export FARGATE_URL="http://lsc-knn-alb-695705521.us-east-1.elb.amazonaws.com"           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
-export EC2_URL="http://18.206.59.157:8080"               # e.g. http://<public-ip>:8080
+export LAMBDA_ZIP_URL=""        # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export LAMBDA_CONTAINER_URL=""  # e.g. https://<id>.lambda-url.us-east-1.on.aws
+export FARGATE_URL=""           # e.g. http://<alb-dns>.us-east-1.elb.amazonaws.com
+export EC2_URL=""               # e.g. http://<public-ip>:8080
